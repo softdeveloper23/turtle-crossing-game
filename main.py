92a718fdcbcdd7cc8d1f1487cfbef853118a3cdc
softdeveloper23 <brannongarrett77@gmail.com>
2025-07-03 20:19:51 +0200

    Added player functionality and movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from turtle import Screen
+from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
@@ -7,6 +7,11 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.bgcolor("black")
+
+player = Player()
+screen.listen()
+screen.onkey(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
--- player.py
+++ player.py
@@ -8,3 +8,11 @@ FINISH_LINE_Y = 280
 class Player(Turtle):
     def __init__(self) -> None:
         super().__init__()
+        self.setheading(90)
+        self.shape("turtle")
+        self.color("green")
+        self.penup()
+        self.goto(0, -280)
+
+    def move(self):
+        self.forward(10)
