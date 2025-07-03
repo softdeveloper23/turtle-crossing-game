03456160c50731cdb324fa34ff9c9b2c0e2d8515
softdeveloper23 <brannongarrett77@gmail.com>
2025-07-03 18:43:19 +0200

    Added starting code and imports

--- player.py
+++ player.py
@@ -1,7 +1,10 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
